a4ba663cf626a364e73b065da3014a03c0226eda
Andrii Krasovskyi <arni.if@gmail.com>
2020-08-13 19:04:19 +0300

    start

---
+++ TestProjectForSonarQubeAnalyze/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TestProjectForSonarQubeAnalyze
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Hello " + textBox1.Text + "!!!");
+        }
+
+        public void RemoveText(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "Insert your name")
+            {
+                textBox1.Text = "";
+            }
+        }
+
+        public void AddText(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(textBox1.Text))
+                textBox1.Text = "Insert your name";
+        }
+    }
+}
---
+++ TestProjectForSonarQubeAnalyze/Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProjectForSonarQubeAnalyze
+{
+    class Person
+    {
+        public string name;
+        public int age;
+
+        public void Go()
+        {
+            var x = name + age;
+        }
+
+        public void Run()
+        {
+            var x = name + age;
+        }
+    }
+}
